9cb4a81776cc76e8e7e2ab865148f9fad57526de
blahthebiste <eliezermiron116@gmail.com>
2021-08-23 20:49:43 -0700

    Fixed lightning delay when not in fast mode

--- main/java/theShadeThatFades/actions/LightningAction.java
+++ main/java/theShadeThatFades/actions/LightningAction.java
@@ -129,15 +129,15 @@ public class LightningAction extends AbstractGameAction {
         if (target.hasPower(LockOnPower.POWER_ID)) {
             this.damage = (int)((float)this.damage * 1.5F);
         }
-        this.addToTop(new DamageAction(this.target, new DamageInfo(this.source, this.damage, this.type)));
-        this.addToTop(new VFXAction(new CustomLightningEffect(this.target.drawX, this.target.drawY, this.customColor)));
+        this.addToBot(new SFXAction("ORB_LIGHTNING_EVOKE", 0.1F));
+        this.addToBot(new VFXAction(new CustomLightningEffect(this.target.drawX, this.target.drawY, this.customColor)));
 //        this.addToTop(new VFXAction(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, this.attackEffect)));
-        this.addToTop(new SFXAction("ORB_LIGHTNING_EVOKE", 0.1F));
+        this.addToBot(new DamageAction(this.target, new DamageInfo(this.source, this.damage, this.type)));
     }
 
     public void update() {
         if (!Settings.FAST_MODE) {
-            this.addToTop(new WaitAction(0.1F));
+            this.addToBot(new WaitAction(0.1F));
         }
 
         if (targetsAll) {
